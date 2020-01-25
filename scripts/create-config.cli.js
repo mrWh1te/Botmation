549dad46a3b4bd5cbde50f2758650c30729c4eb9
Michael Lage <gs.urmomma@gmail.com>
2020-01-24 17:30:11 -0700

    Replaced readline with readline-sync
    - easier interface to work with

--- scripts/create-config.cli.js
+++ scripts/create-config.cli.js
@@ -1,21 +1,15 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+var readlineSync = require('readline-sync');
 
 // 1. Detect if the config file has already been created
 
 // 2. If it hasn't then run this script, else end
-
-rl.question("What is your name ? ", function(name) {
-    rl.question("Where do you live ? ", function(country) {
-        console.log(`${name}, is a citizen of ${country}`);
-        rl.close();
-    });
+ 
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
+// Handle the secret text (e.g. password).
+var favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
 });
-
-rl.on("close", function() {
-    console.log("\nBYE BYE !!!");
-    process.exit(0);
-});
+console.log('Oh, ' + userName + ' loves ' + favFood + '!');

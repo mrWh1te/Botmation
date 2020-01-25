4975751e3ad91fd133128469173ac5e6aed169ab
Michael Lage <gs.urmomma@gmail.com>
2020-01-25 11:24:10 -0700

    lets promote reusable patterns
    - lets maintain less code
    - less is more
    - less makes pandas around the world cry tears of joy

--- src/bots/instagram/actions/auth.ts
+++ src/bots/instagram/actions/auth.ts
@@ -48,11 +48,5 @@ export const isLoggedIn = async(tab: puppeteer.Page): Promise<boolean> => {
   return tab.url() !== getInstagramLoginUrl()
 }
 
-export const isGuest = async(tab: puppeteer.Page): Promise<boolean> => {
-  // Go to the login page
-  await goTo(getInstagramLoginUrl())(tab)
-  
-  // if you're logged in, Instagram would have redirected you to the feed
-  // if you were a guest, logged out, you would be on the Instagram Login URL
-  return tab.url() === getInstagramLoginUrl()
-}
+export const isGuest = async(tab: puppeteer.Page): Promise<boolean> =>
+  !(await isLoggedIn(tab))

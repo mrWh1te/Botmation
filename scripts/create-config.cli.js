f308e3cc0ba47637a3baf358460b91305aca8dd8
Michael Lage <gs.urmomma@gmail.com>
2020-01-24 19:00:34 -0700

    Postinstall CLI script working
    - this CLI script walks you through collecting important information to automatically generate the config file for you
    - the CLI script will first check for the config file, and run the prompt only if the file is missing
    - if the config file is missing, it asks the user if they want to go through the prompt flow, since they might be using the puppeteer-cluster, which in that case, each bot is configured through code, not relying on this file. That said, this helps the user get the example bot running even faster

--- scripts/create-config.cli.js
+++ scripts/create-config.cli.js
@@ -1,15 +1,100 @@
-var readlineSync = require('readline-sync');
+var readlineSync = require('readline-sync')
+var fs = require('fs')
+
+const configFilePath = './src/config.ts'
+
+// Header Print
+console.log('#######################################')
+console.log('## Botmation: CLI Create "config.ts" ##')
+console.log('#######################################')
+emptyLine()
 
 // 1. Detect if the config file has already been created
+try {
+  if (fs.existsSync(configFilePath)) {
+    // file exists, do nothing
+    console.log('[NOTICE] File: "./src/config.ts" exists\n')
+    completeSuccess()
+  } else {
+    console.log('[NOTICE] File: "./src/config.ts" is missing\n')
+    createConfigFileCLIWalkthrough()
+  }
+} catch(err) {
+  console.error(err)
+}
 
-// 2. If it hasn't then run this script, else end
- 
-// Wait for user's response.
-var userName = readlineSync.question('May I have your name? ');
-console.log('Hi ' + userName + '!');
- 
-// Handle the secret text (e.g. password).
-var favFood = readlineSync.question('What is your favorite food? ', {
-  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
-});
-console.log('Oh, ' + userName + ' loves ' + favFood + '!');
+function createConfigFileCLIWalkthrough() {
+  // 2. If it hasn't then run this script, else end
+  if (readlineSync.keyInYN('Do you want to create it now?')) {
+    // 'Y' key was pressed.
+    okay()
+    
+    //
+    // Instagram Auth Credentials
+    var instagramUsername = readlineSync.question('What is your Instagram username ? ')
+    okay()
+    
+    // Handle the secret text (e.g. password).
+    var instagramPassword = readlineSync.question('What is your Instagram password ? ', {
+      hideEchoBack: true
+    })
+    okay()
+    
+    //
+    // Cookies Directory Path
+    var cookiesDirectory = readlineSync.question('Directory for cookies (./assets/cookies/) ? ') || '/assets/cookies/'
+    okay()
+
+    //
+    // Data collected, now let's create the config file
+    saveConfigFile(getConfigFileText(instagramUsername, instagramPassword, cookiesDirectory))
+  } else {
+    // Another key was pressed.
+    console.log('Okay, good bye')
+  }
+}
+
+//
+// Helper functions
+function emptyLine() {
+  console.log('')
+}
+function okay() {
+  console.log('Okay.\n')
+}
+function completeSuccess() {
+  console.log('All set, good bye!\n')
+}
+
+function saveConfigFile(fileText) {
+  console.log('fileText = '+fileText)
+
+  fs.writeFile(configFilePath, fileText, 'utf8', function (err) {
+    if (err) {
+      console.log('[ERROR] unable to create "./src/config.ts"\n')
+    }
+
+    console.log('[NOTICE] "./src/config.ts" has been created!\n')
+    completeSuccess()
+  });
+}
+
+//
+// Templates
+function getConfigFileText(username, password, cookiesDirectory) {
+  return `
+  /**
+   * Main Configuration to setup the Bot
+   */
+
+  export const ACCOUNT_USERNAME = '${username}'
+  export const ACCOUNT_PASSWORD = '${password}'
+
+  // TODO: determine how to manage auth of multiple bots
+
+  /**
+   * @note from the root project directory
+   * @default   \`./assets/cookies/\`
+   */
+  export const COOKIES_DIRECTORY_PATH = '${cookiesDirectory}'`
+}

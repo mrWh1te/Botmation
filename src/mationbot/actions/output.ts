ec78519b8c16ddc8ec6beb77bf648f306296d530
Michael Lage <gs.urmomma@gmail.com>
2020-01-25 19:43:39 -0700

    Expirmental screenshotAll() action
    - the commented out code works for sure, but have yet to test the code left in, and needs polish, but as a way to bypass the usage of a factory for one action to wrap another action by invoking the factory of the action being wrapped to return an action

--- src/mationbot/actions/output.ts
+++ src/mationbot/actions/output.ts
@@ -3,6 +3,10 @@ import puppeteer from 'puppeteer'
 import { BotAction } from "@mationbot/interfaces/bot-action.interfaces"
 import { getPageScreenshotLocalFileUrl } from '@helpers/assets'
 
+import { forAll } from '@mationbot/actions/utilities'
+import { BotActionsChainFactory } from '@mationbot/factories/bot-actions-chain.factory'
+import { goTo } from '@mationbot/actions/navigation'
+
 /**
  * @description   Take a PNG screenshot of the current page
  * @param fileName name of the file to save the PNG as
@@ -10,3 +14,26 @@ import { getPageScreenshotLocalFileUrl } from '@helpers/assets'
 export const screenshot = (fileName: string): BotAction => async(tab: puppeteer.Page) => {
   await tab.screenshot({path: getPageScreenshotLocalFileUrl(`${fileName}.png`)})
 }
+
+/**
+ * @description    
+ * @param sites ['example.com', 'whatever.com']
+ */
+export const screenshotAll = (...sites: string[]): BotAction => async(tab: puppeteer.Page) =>
+  await forAll(sites)( // await needed?
+    (siteName) => ([
+      goTo('http://' + siteName),
+      screenshot(siteName+'-homepage')
+    ])
+  )(tab) 
+
+  // Either use the Factory to inject the tab, with the advantage of providing a list of actions
+  // or do above, inject the tab yourself, and skip the factory since you do not need a chain to run 1 forAll() action
+  // BotActionsChainFactory(tab)(
+  //   forAll(sites)(
+  //     (siteName) => ([
+  //       goTo('http://' + siteName),
+  //       screenshot(siteName+'-homepage')
+  //     ])
+  //   ) 
+  // )

1ed46d022f42410433f7052b0f6451ab2f5203c9
Michael Lage <gs.urmomma@gmail.com>
2020-01-31 20:05:36 -0700

    WIP utilities test suite
    - skipping sleep() for jest new feature in support of promisified setTimeout and their useFakeTimers()
    - unit-test for givenThat

---
+++ src/tests/mationbot/actions/utilites.spec.ts
@@ -0,0 +1,67 @@
+import { Page } from 'puppeteer'
+
+import { getDefaultGoToPageOptions } from '@mationbot/helpers/navigation'
+
+import { BASE_URL } from '@tests/urls'
+import { givenThat } from '@mationbot/actions/utilities'
+import { click, type } from '@mationbot/actions/input'
+
+/**
+ * @description   Utilities Action Factory
+ *                The factory methods here return BotAction's for the bots to handle more complex use-cases
+ */
+describe('[MationBot:Action Factory] Utilities', () => {
+
+  let mockPage: Page = {
+    click: jest.fn(),
+    keyboard: {
+      type: jest.fn()
+    }
+  } as any as Page
+
+  beforeAll(async() => {
+    await page.goto(BASE_URL, getDefaultGoToPageOptions())
+  })
+
+  //
+  // sleep() Integration Test
+  // it('should call setTimeout with the correct values', async() => {
+    // jest.useFakeTimers()
+    // TBI: jest does not natively support async/await promised based setTimeout
+    //      therefore, there is no simple way to test this. Fortunately, it's a simple Action
+    // Follow here: https://github.com/facebook/jest/issues/7151
+  // })
+
+  //
+  // givenThat() Unit Test
+  it('should resolve the condition and ONLY run the chain of actions of the resolved condition equals TRUE', async() => {
+    const conditionResolvingTRUE = async(page: Page) => new Promise<boolean>(resolve => resolve(true))
+    const conditionResolvingFALSE = async(page: Page) => new Promise<boolean>(resolve => resolve(false))
+    // const conditionReject = async(page: Page) => new Promise((resolve, reject) => reject()) // TODO: 
+
+    await givenThat(conditionResolvingTRUE)(
+      click('example selector 1'),
+      type('example copy 1')
+    )(mockPage)
+
+    await givenThat(conditionResolvingFALSE)(
+      click('example selector 2'),
+      type('example copy 2')
+    )(mockPage)
+
+    expect(mockPage.click).toHaveBeenNthCalledWith(1, 'example selector 1')
+    expect(mockPage.keyboard.type).toHaveBeenNthCalledWith(1, 'example copy 1')
+
+    expect(mockPage.click).not.toHaveBeenNthCalledWith(2, 'example selector 2')
+    expect(mockPage.keyboard.type).not.toHaveBeenNthCalledWith(2, 'example copy 2')
+  })
+
+  //
+  // forAll() Unit Test
+  it('should call the list of Actions for each item in the array provided', async() => {
+    expect(1).toEqual(1)
+  })
+  it('should call the list of Actions for each key->value pair in the object provided', async() => {
+    expect(1).toEqual(1)
+  })
+})

f179bd720f5245e158be9c0b2c5cd2d7c8f7f45e
Michael Lage <gs.urmomma@gmail.com>
2020-02-24 13:02:52 -0700

    Standardized the BotmationInterface
    - Future: replace closePage() with a bot action, it can stay a class
      method, but not required in the interface
    - Resolves Issue #13

--- src/botmation/interfaces/botmation.interface.ts
+++ src/botmation/interfaces/botmation.interface.ts
@@ -1,9 +1,21 @@
+import { Page } from 'puppeteer'
 import { BotAction } from './bot-action.interfaces'
+import { BotOptions } from './bot-options.interfaces'
 
 /**
  * @description   Base public interface for a Botmation instance
+ *                This is the expected public interface of methods
+ *                  in case anyone wants to create a more specific Botmation "class" without using inheritance
  */
 export interface BotmationInterface {
+  // Main
   actions(...actions: BotAction[]): Promise<void>
-  closePage(): Promise<void>
+  // Page
+  setPage(page: Page): void
+  getPage(): Page
+  // Options
+  setOptions(options: Partial<BotOptions>): void
+  updateOptions(options: Partial<BotOptions>): void
+  // Injects are optional class code
+  // closePage is optional TODO: add a closePage() bot action
 }

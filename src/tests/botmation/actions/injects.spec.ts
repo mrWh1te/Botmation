d66c1b692739305d518d50f7395c11eb96692e86
Michael Lage <gs.urmomma@gmail.com>
2020-07-25 13:49:58 -0600

    Testing - actions/injects
    - TIL to mock a module being imported to adjust an exported function
      that is used in another function being tested
    - we test the inner assemblyLine()()() call within the injects()() given
      various injects (new and past) - unit test

---
+++ src/tests/botmation/actions/injects.spec.ts
@@ -0,0 +1,54 @@
+
+
+import { injects } from "botmation/actions/injects"
+import { Page } from "puppeteer"
+
+// Mock the Assembly-Lines Module so when injects() imports it
+// we can test how injects() calls the exported assemblyLine higher order function
+// particularly, interested in the inner 3rd call, the actual BotAction to determine 
+// if or not we have properly set up the injects based on the provided injects, both old and new
+const mockAssemblyLineBotAction = jest.fn()
+jest.mock('botmation/actions/assembly-lines', () => {
+  // Require the original module to not be mocked...
+  const originalModule = jest.requireActual('botmation/actions/assembly-lines');
+
+  return {
+    // __esModule: true, // Use it when dealing with esModules
+    ...originalModule,
+    assemblyLine: () => () => mockAssemblyLineBotAction
+  }
+})
+
+/**
+ * @description   Injects BotAction
+ */
+describe('[Botmation] actions/injects', () => {
+
+  const newInjects = ['new1', 'new2']
+  const pastInjects = ['past1', 'past2', 'past3']
+
+  let mockPage = {} as any as Page
+  
+  //
+  // Basic Integration Tests
+  it('should set the injects of the inner assembly-line with the new ones at the start', async () => {
+    // no injects
+    await injects()()(mockPage)
+
+    // new injects, but no past injects
+    await injects(...newInjects)()(mockPage)
+
+    // new injects, and past injects
+    await injects(...newInjects)()(mockPage, ...pastInjects)
+    
+    expect(mockAssemblyLineBotAction).toHaveBeenNthCalledWith(1, {})
+    expect(mockAssemblyLineBotAction).toHaveBeenNthCalledWith(2, {}, 'new1', 'new2')
+    expect(mockAssemblyLineBotAction).toHaveBeenNthCalledWith(3, {}, 'new1', 'new2', 'past1', 'past2', 'past3')
+  })
+
+  afterAll(() => {
+    // unmock the module for other tests
+    jest.unmock('botmation/actions/assembly-lines')
+  })
+  
+})

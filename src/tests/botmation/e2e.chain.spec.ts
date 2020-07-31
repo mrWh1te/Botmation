1926143bd3681b0967e1519084ea755a9b41e2a1
Michael Lage <gs.urmomma@gmail.com>
2020-07-30 19:15:33 -0600

    Increase Testing Stability
    - seems like there is an issue with running e2e tests inside a
      Puppeteer Page in parallel causes issues, browser disconnect?
    - so for now, limiting e2e Puppeteer Page test to 1 per test suite and
      running tests with run in band, so 1 suite at a time
    - will follow the jest-puppeteer related issue
    - might switch to a /e2e folder to put e2e tests for everything inside
      it like 1 file per BotAction

---
+++ src/tests/botmation/e2e.chain.spec.ts
@@ -0,0 +1,36 @@
+import 'expect-puppeteer'
+
+import { click, type } from 'botmation/actions/input'
+import { goTo, waitForNavigation } from 'botmation/actions/navigation'
+
+import { BASE_URL, EXAMPLE_URL } from '../urls'
+import { FORM_TEXT_INPUT_SELECTOR, FORM_SUBMIT_BUTTON_SELECTOR } from '../selectors'
+import { chain } from 'botmation/actions/assembly-lines'
+import { errors } from 'botmation/actions/errors'
+
+/**
+ * @description   E2E
+ */
+describe('[Botmation] e2e chain()', () => {
+
+  beforeEach(async() => {
+    await goTo(BASE_URL)(page)
+  })
+
+  //
+  // Functional
+  it('should run a chain of actions which submits the example form with errors() wrapping', async() => {
+    await chain(
+      errors('chain-test')(
+        goTo(EXAMPLE_URL),
+        click(FORM_TEXT_INPUT_SELECTOR),
+        type('loremlipsum2'),
+        click(FORM_SUBMIT_BUTTON_SELECTOR),
+        waitForNavigation
+      )
+    )(page)
+
+    expect(page.url()).toEqual('http://localhost:8080/success.html?answer=loremlipsum2')
+  })
+
+})
---
+++ src/tests/botmation/e2e.class.spec.ts
@@ -0,0 +1,35 @@
+import 'expect-puppeteer'
+
+import { click, type } from 'botmation/actions/input'
+import { goTo, waitForNavigation } from 'botmation/actions/navigation'
+import { Botmation } from 'botmation/class'
+
+import { BASE_URL, EXAMPLE_URL } from '../urls'
+import { FORM_TEXT_INPUT_SELECTOR, FORM_SUBMIT_BUTTON_SELECTOR } from '../selectors'
+
+/**
+ * @description   E2E
+ */
+describe('[Botmation] e2e Botmation class', () => {
+
+  beforeEach(async() => {
+    await goTo(BASE_URL)(page)
+  })
+
+  //
+  // Botmation Class Instance
+  it('should create a Botmation instance then run the actions to submit the example form', async() => {
+    const bot = new Botmation(page)
+
+    await bot.actions(
+      goTo(EXAMPLE_URL),
+      click(FORM_TEXT_INPUT_SELECTOR),
+      type('loremlipsum'),
+      click(FORM_SUBMIT_BUTTON_SELECTOR),
+      waitForNavigation
+    )
+
+    expect(page.url()).toEqual('http://localhost:8080/success.html?answer=loremlipsum')
+  })
+
+})

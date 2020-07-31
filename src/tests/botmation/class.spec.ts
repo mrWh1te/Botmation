03d08d16ff94889031fb40a56698ed6593d29e71
Michael Lage <gs.urmomma@gmail.com>
2020-07-30 18:01:20 -0600

    Test Improvements

--- src/tests/botmation/class.spec.ts
+++ src/tests/botmation/class.spec.ts
@@ -1,13 +1,10 @@
-import 'expect-puppeteer'
+// import 'expect-puppeteer'
 import { Page, Browser } from 'puppeteer'
 
 import { enrichGoToPageOptions } from 'botmation/helpers/navigation'
-import { click, type } from 'botmation/actions/input'
-import { goTo, waitForNavigation } from 'botmation/actions/navigation'
+import { click } from 'botmation/actions/input'
+import { goTo } from 'botmation/actions/navigation'
 import { Botmation } from 'botmation/class'
-
-import { BASE_URL, EXAMPLE_URL } from '../urls'
-import { FORM_TEXT_INPUT_SELECTOR, FORM_SUBMIT_BUTTON_SELECTOR } from '../selectors'
 
 /**
  * @description   Test the Botmation class
@@ -50,23 +47,6 @@ describe('[Botmation] class', () => {
     } as any as Browser
     const bot2 = await Botmation.asyncConstructor(mockBrowserHasPages)
     expect(bot2.getPage()).toEqual('page1')
-  })
-
-  //
-  // Regular Class Instance
-  it('should create a Botmation instance then run the actions', async() => {
-    const bot = new Botmation(page)
-
-    await bot.actions(
-      goTo(BASE_URL),
-      goTo(EXAMPLE_URL),
-      click(FORM_TEXT_INPUT_SELECTOR),
-      type('loremlipsum'),
-      click(FORM_SUBMIT_BUTTON_SELECTOR),
-      waitForNavigation
-    )
-
-    expect(page.url()).toEqual('http://localhost:8080/success.html?answer=loremlipsum')
   })
 
   //

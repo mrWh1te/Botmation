ec529d5ee1fc6cc5d5a6d824d7748df792b5f0b8
Michael Lage <gs.urmomma@gmail.com>
2020-07-30 19:53:57 -0600

    Tests passing locally ?

--- src/tests/botmation/actions/assembly-lines.chain.e2e.spec.ts
+++ src/tests/botmation/actions/assembly-lines.chain.e2e.spec.ts
@@ -16,16 +16,18 @@ describe('[Botmation] actions/assembly-lines chain() e2e', () => {
   //
   // Functional
   it('should run a chain of actions which submits the example form with errors() wrapping', async() => {
-    await chain(
-      // errors('chain-test')(
-        goTo(BASE_URL),
-        goTo(EXAMPLE_URL),
-        click(FORM_TEXT_INPUT_SELECTOR),
-        type('loremlipsum2'),
-        click(FORM_SUBMIT_BUTTON_SELECTOR),
-        waitForNavigation
-      // )
-    )(page)
+    try {
+      await chain(
+        // errors('chain-test')(
+          goTo(BASE_URL),
+          goTo(EXAMPLE_URL),
+          click(FORM_TEXT_INPUT_SELECTOR),
+          type('loremlipsum2'),
+          click(FORM_SUBMIT_BUTTON_SELECTOR),
+          waitForNavigation
+        // )
+      )(page)
+    } catch (error) {}
 
     expect(page.url()).toEqual('http://localhost:8080/success.html?answer=loremlipsum2')
   })

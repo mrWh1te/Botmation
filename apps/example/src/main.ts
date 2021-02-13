e1aa9f112e4104556e49b0999af462e25fbd7baa
Michael Lage <lage.michael@gmail.com>
2021-02-13 15:09:38 -0700

    core project is default project & polish example
    - remove /assets from example build

--- apps/example/src/main.ts
+++ apps/example/src/main.ts
@@ -2,44 +2,42 @@
  * @description  Simple Example Script
  */
 import * as puppeteer from 'puppeteer'
-import { chain } from '@botmation/core'
 
-// General BotAction's
-import { log } from '@botmation/core'
-import { goTo } from '@botmation/core'
-import { screenshot } from '@botmation/core'
-import { logError } from '@botmation/core'
+import {
+  chain,
+  goTo,
+  screenshot,
+  log,
+  logError
+} from '@botmation/core'
 
-import { login } from '@botmation/instagram'
 
 (async () => {
   let browser: puppeteer.Browser
+  let page: puppeteer.Page
 
   try {
-    // Get the Browser from Puppeteer
-    browser = await puppeteer.launch({headless: false}) // {headless: false} shows the browser, run in headless if you don't care to see
+    // Get a Browser instance from Puppeteer
+    browser = await puppeteer.launch({headless: false}) // headless = false => shows the browser; true => runs the code without displaying the browser (headless mode)
 
     // Grab a Page from the Browser for the bot to operate in
     const pages = await browser.pages()
-    const page = pages.length === 0 ? await browser.newPage() : pages[0]
+    page = pages.length === 0 ? await browser.newPage() : pages[0]
 
     // Run the chain of actions
     await chain(
-      log('Bot running'),
-      screenshot('before-login'),
-      goTo('https://www.instagram.com/accounts/login/'),
-      screenshot('insta-login'),
-      login({username: 'instagram-username', password: 'instagram-password'}),
-      screenshot('after-login'),
+      log('Bot is running'),
+      goTo('https://www.duckduckgo.com/'),
+      screenshot('duckduckgo-homepage'),
       log('Screenshot taken')
     )(page)
 
-    await browser.close()
+    await page.close()
   } catch (error) {
     logError(error)
 
     setTimeout(async() => {
-      if (browser) await browser.close()
+      if (page) await page.close()
     })
   }
 

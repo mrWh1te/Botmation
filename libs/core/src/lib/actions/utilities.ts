9a3116cb1c9d912addf98780fc634cd47e6266dc
Michael Lage <lage.michael@gmail.com>
2021-02-17 19:57:08 -0700

    cron-schedule & template string literals for crontab

--- libs/core/src/lib/actions/utilities.ts
+++ libs/core/src/lib/actions/utilities.ts
@@ -13,6 +13,8 @@ import { PipeValue } from '../types/pipe-value'
 import { AbortLineSignal, isAbortLineSignal } from '../types/abort-line-signal'
 import { processAbortLineSignal } from '../helpers/abort'
 
+import { parseCronExpression } from "cron-schedule";
+
 /**
  * @description Higher Order BotAction that accepts a ConditionalBotAction (pipeable, that returns a boolean) and based on what boolean it resolves,
  *              does it run the BotAction's provided in a pipe()(). Never the less, it does not return the final ran BotAction return value (if any)
@@ -216,9 +218,27 @@ export const forAsLong =
  *
  * @param schedule
  */
+type wildcard = '*'
+type anyValue = wildcard | number
+
+type rangeOfValues = `${number}-${number}`
+type stepValues = `${rangeOfValues}/${number}` | `${anyValue}/${number}`
+
+type minute = anyValue // 0-59 | *
+type hour = anyValue | stepValues // 0-23
+type dayOfMonth = anyValue | stepValues // 1-31
+type month = anyValue | stepValues | 'jan' | 'feb' | 'mar' | 'apr' | 'may' | 'jun' | 'jul' | 'aug' | 'sep' | 'oct' | 'nov' | 'dec' | 'JAN' | 'FEB' | 'MAR' | 'APR' | 'MAY' | 'JUN' | 'JUL' | 'AUG' | 'SEP' | 'OCT' | 'NOV' | 'DEC'
+type weekDay = anyValue | stepValues | 'mon' | 'tue' | 'wed' | 'thu' | 'fri' | 'sat' | 'sun' | 'MON' | 'TUE' | 'WED' | 'THU' | 'FRI' | 'SAT' | 'SUN'
+
+type crontab = `${minute} ${hour} ${dayOfMonth} ${month} ${weekDay}`
+
 export const schedule =
-  (schedule: string) =>
+  (schedule: crontab) =>
     (...actions: BotAction[]): BotAction<any> =>
       async(page, ...injects) => {
+        // 1. check schedule
+        const cron = parseCronExpression('')
+        // 2. timeout until scheduled run time (with actions set to run)
 
+        // 3. after actions run, reschedule if needed
       }

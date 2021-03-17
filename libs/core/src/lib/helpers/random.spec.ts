7c2467c71cda7fc016b2c0e149566a1810c18f2a
Michael Lage <lage.michael@gmail.com>
2021-03-16 21:26:07 -0600

    delete randomDiceRoll()
    - no longer necessary

--- libs/core/src/lib/helpers/random.spec.ts
+++ libs/core/src/lib/helpers/random.spec.ts
@@ -1,39 +1,10 @@
-import { randomDiceRoll, randomDecimal } from './random'
+import { randomDecimal } from './random'
 
 /**
  * @description   Random Helpers
  *  These functions focus on randomness
  */
 describe('[Botmation] helpers/random', () => {
-
-  //
-  // Unit Test
-  it('randomDiceRoll() returns a random number from 1 to n where n is the numberOfDiceSides', () => {
-    const noParams = randomDiceRoll()
-    const diceWithTwoSides = randomDiceRoll(2)
-    const diceWithNineSides = randomDiceRoll(9)
-    const diceWithTwentyFiveSides = randomDiceRoll(25)
-
-    expect(noParams).toEqual(1)
-
-    expect(diceWithTwoSides).toBeGreaterThanOrEqual(1)
-    expect(diceWithTwoSides).toBeLessThanOrEqual(2)
-
-    expect(diceWithNineSides).toBeGreaterThanOrEqual(1)
-    expect(diceWithNineSides).toBeLessThanOrEqual(9)
-
-    expect(diceWithTwentyFiveSides).toBeGreaterThanOrEqual(1)
-    expect(diceWithTwentyFiveSides).toBeLessThanOrEqual(25)
-
-    // unique edge cases
-    const diceWithZeroSides = randomDiceRoll(0)
-    const diceWithNegativeOneSide = randomDiceRoll(-1)
-    const diceWithNegativeSides = randomDiceRoll(-10)
-
-    expect(diceWithZeroSides).toEqual(0)
-    expect(diceWithNegativeOneSide).toEqual(0)
-    expect(diceWithNegativeSides).toEqual(0)
-  })
 
   it('randomDecimal() returns a pseudorandom decimal between 0 to 1', () => {
     for(let i = 0; i < 100; i++) {
--- libs/core/src/lib/helpers/random.ts
+++ libs/core/src/lib/helpers/random.ts
@@ -1,15 +1,4 @@
 /**
- * Roll a virtual dice with n number of sides then return the side's number that it lands on
- * @param numberOfDiceSides n where n >= 1
- *  if n is 0 or negative, then return value is 0
- * @return 1 -> n (including n)
- */
-export const randomDiceRoll = (numberOfDiceSides = 1): number =>
-  numberOfDiceSides >= 1 ?
-  Math.floor(randomDecimal() * numberOfDiceSides) + 1 :
-  0
-
-/**
  * @return decimal between 0-1
  */
 export const randomDecimal = (): number =>

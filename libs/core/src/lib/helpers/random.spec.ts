b5da118c1a2188fb4b069b37bbbdf9600f954d5b
Michael Lage <lage.michael@gmail.com>
2021-03-16 21:37:35 -0600

    refine test expectations

--- libs/core/src/lib/helpers/random.spec.ts
+++ libs/core/src/lib/helpers/random.spec.ts
@@ -9,8 +9,8 @@ describe('[Botmation] helpers/random', () => {
   it('randomDecimal() returns a pseudorandom decimal between 0 to 1', () => {
     for(let i = 0; i < 100; i++) {
       const randomDecimalValue = randomDecimal()
-      expect(randomDecimalValue).toBeGreaterThanOrEqual(0)
-      expect(randomDecimalValue).toBeLessThanOrEqual(1)
+      expect(randomDecimalValue).toBeGreaterThan(0)
+      expect(randomDecimalValue).toBeLessThan(1)
     }
   })
 

42f9feb517bca2b9480fd3306eb8d926dbc893d8
Michael Lage <lage.michael@gmail.com>
2021-03-16 21:45:22 -0600

    notes

--- libs/core/src/lib/actions/random.ts
+++ libs/core/src/lib/actions/random.ts
@@ -17,8 +17,8 @@ export const rollDice =
  * Run assembled BotActions based on a probability
  *  if random number generated is within the probability range, then assembled BotActions are ran
  *  ie if probability is 60% then all numbers generated 0-60 will cause actions to run and all numbers generated 61-100 will cause nothing
- * @future use givenThat() with a wrapper function post v2 (support sync BotActions and )
- * @param probability decimal between 0-1 ie .6 = 60% probability of assembled BotActions running
+ * @future use givenThat() with a wrapper function post v2 (support sync BotActions and decomposed `page` from params into an inject)
+ * @param probability represented as a decimal ie .6 = 60% chance of running assembled BotActions
  * @param generateRandomDecimal function to generate a random decimal between 0 and 1 with default set to randomDecimal helper that uses a pseudo random method
  */
 export const probably =

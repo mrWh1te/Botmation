d32ce8ae5ed774d90b424db5ba60d35f6a0b8d29
Michael Lage <lage.michael@gmail.com>
2021-07-08 15:40:54 -0600

    clean up concept

--- apps/puppeteer-bot/src/main.ts
+++ apps/puppeteer-bot/src/main.ts
@@ -8,25 +8,11 @@ import {
 
 (async () => {
   try {
-    // // Get a Browser instance from Puppeteer
-    // browser = await puppeteer.launch({headless: false}) // headless = false => shows the browser; true => runs the code without displaying the browser (headless mode)
-
-    // // Grab a Page from the Browser for the bot to operate in
-    // const pages = await browser.pages()
-    // page = pages.length === 0 ? await browser.newPage() : pages[0]
-
-    // // Compose the bot
-    // const bot = chain(
-    //   log('Bot is running'),
-    //   goTo('https://www.duckduckgo.com/'),
-    //   screenshot('duckduckgo-homepage'),
-    //   log('Screenshot taken')
-    // )
-
-    // concept
+    // Inject Puppeteer browser instance
     const bot = browser({headless: false})(
+      // Inject Puppeteer browser page instance
       browserPage()(
-        // has inject `page` for BotActions that rely on Puppeteer.page working instance
+        // has inject `page` (& 'browser') for assembled Actions that rely on Puppeteer.page
         async({page}) => {
           await page.goto('https://duckduckgo.com')
           await sleep(10000)
